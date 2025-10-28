153e4e4e6a289301376009dca04636ef4a9f3c23
AHMED HAFDI <58536023+HAFDIAHMED@users.noreply.github.com>
2025-10-28 15:07:33 +0100

    Fix formatting in script_push.bat

--- script_push.bat
+++ script_push.bat
@@ -3,7 +3,10 @@
 C:\Users\018801\Documents\github\hafdiahmed.github.io
 echo "Automatic commit at %date% %time%" > commit_message.txt
 git pull
-git add .
+git add .    
+
+
+
 git commit -m "Automatic commit at %date% %time%"
 git push -f
 timeout /t 100 /nobreak
